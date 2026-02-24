229d82afb7b7b80ea1756d98e4c755142270487d
nupurgandhi4120-sys <nupurgandhi4120@gmail.com>
2026-02-24 11:12:27 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+#include
+using namespace std;
+
+int main() {
+// Write C++ code here
+int a=20;
+
+if(a%2==0){
+    cout << "the number is even";
+}
+else{
+    cout << "the number id odd";
+}
+
+return 0;
+}`
