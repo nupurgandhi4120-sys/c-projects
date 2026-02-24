be050e9da03047a73df3d8c4971b5c26ecc151a3
nupurgandhi4120-sys <nupurgandhi4120@gmail.com>
2026-02-24 11:08:37 +0530

    Create arithamtic.cpp

---
+++ arithamtic.cpp
@@ -0,0 +1,13 @@
+#include
+
+int main() {
+using namespace std;
+int a=6, b=2;
+
+cout << "sum = " << a+b << endl;
+cout << "difference = " << a-b << endl;
+int m=a/b;
+cout << "m = " << m;
+
+return 0;
+}
